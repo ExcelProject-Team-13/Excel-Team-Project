d99f9353117efe4e115833591e57977911aaf4f1
AgheraRaj <rajaghera2004@gmail.com>
2025-05-14 19:43:33 +0530

    landing page

--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -6,7 +6,8 @@ import {
   User,
   AtSign,
   Mail,
-  Lock
+  Lock,
+  ChartBarStacked
 } from "lucide-react"
 import { GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
 import axios from "axios";
@@ -164,7 +165,7 @@ const SignUpPage = () => {
           {/* LOGO */}
           <div className="text-center mb-5">
             <div className="flex flex-col items-center gap-2 group">
-              <svg id="logo-15" width="35px" height="35px" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M24.5 12.75C24.5 18.9632 19.4632 24 13.25 24H2V12.75C2 6.53679 7.03679 1.5 13.25 1.5C19.4632 1.5 24.5 6.53679 24.5 12.75Z" className="ccustom" fill="#126a4e"></path> <path d="M24.5 35.25C24.5 29.0368 29.5368 24 35.75 24H47V35.25C47 41.4632 41.9632 46.5 35.75 46.5C29.5368 46.5 24.5 41.4632 24.5 35.25Z" className="ccustom" fill="#126a4e"></path> <path d="M2 35.25C2 41.4632 7.03679 46.5 13.25 46.5H24.5V35.25C24.5 29.0368 19.4632 24 13.25 24C7.03679 24 2 29.0368 2 35.25Z" className="ccustom" fill="#0f4736"></path> <path d="M47 12.75C47 6.53679 41.9632 1.5 35.75 1.5H24.5V12.75C24.5 18.9632 29.5368 24 35.75 24C41.9632 24 47 18.9632 47 12.75Z" className="ccustom" fill="#0f4736"></path> </svg>
+              <ChartBarStacked color="#0f4736" size={32} />
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free account</p>
             </div>
